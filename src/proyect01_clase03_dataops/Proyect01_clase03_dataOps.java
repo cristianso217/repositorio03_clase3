a6bd6fb228e6da7ab4b910721f6e251c3daa7ccf
Cristian <Cristian@DESKTOP-P759R2J>
2025-06-04 21:16:21 -0500

    clase 03

---
+++ src/proyect01_clase03_dataops/Proyect01_clase03_dataOps.java
@@ -0,0 +1,23 @@
+
+package proyect01_clase03_dataops;
+
+import java.util.Scanner;
+public class Proyect01_clase03_dataOps {
+    public static void main(String[] args) {
+        //declarar variables
+        double num1,num2,suma,promedio;
+        Scanner lectura=new Scanner(System.in);
+        //entrada de datos
+        System.out.print("Ingresar número 1:");
+        num1=lectura.nextDouble();
+        System.out.print("Ingresar número 2:");
+        num2=lectura.nextDouble();        
+        // proceso de datos
+        suma=num1+num2;
+        promedio=suma/2;
+        //salida de datos
+        System.out.println("La suma de los números es:"+suma);
+        System.out.println("El promedio de los números es:"+promedio);
+    }
+    
+}
